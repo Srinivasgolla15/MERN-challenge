161d226b87cf9a1b3fd6a6270b0ea18997379c3b
Srinivasgolla15 <lsrinivas0768@gmail.com>
2025-12-17 21:55:49 +0530

    conditionals

--- day-14/code.js
+++ day-14/code.js
@@ -1,11 +1,22 @@
-let color="red"
+let color="red";
 
 if(color=="red"){
-    console.log("red")
+    console.log("red");
 }
 if(color=="yellow"){
-    console.log("yellow")
+    console.log("yellow");
 }
 if(color=="blue"){
-    console.log("blue")
+    console.log("blue");
+}
+
+
+let string="sappljfh";
+if(string[0]=="a"&&string.length>3){
+    console.log("good string");
+}else if(string.length>3&&string.length<6){
+    console.log("moderate")
+}
+else{
+    console.log("poor")
 }
