3a9b0efedcc23f24d42486026d4876398c478083
Srinivasgolla15 <lsrinivas0768@gmail.com>
2025-12-17 22:28:06 +0530

    conditionals

---
+++ day-14/practice.js
@@ -0,0 +1,24 @@
+// let username=prompt("what's your name :");
+// let userage=prompt("what's your age :");
+// let output= `welcome ${username} and your age is ${userage}`;
+// console.log(output)
+
+let quater=1;
+
+switch(quater){
+    case 1:
+        console.log("jan,feb,mar");
+        break;
+    case 2:
+        console.log("apr,may,june");
+        break;
+    case 3:
+        console.log("jul,aug,sep");
+        break;
+    case 4:
+        console.log("oct,nov,dec");
+        break;
+    default:
+        console.log("please enter quater")
+}
+
