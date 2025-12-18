77a37c3ff5ed3ca3ddcb7a83f559c1501bd09922
Srinivasgolla15 <lsrinivas0768@gmail.com>
2025-12-18 23:20:12 +0530

    day-15 practice and ass

--- day-15/practice.js
+++ day-15/practice.js
@@ -9,11 +9,33 @@
 // console.log(name.slice(4).replace("l","t").replace("l","t"))
 
 
-let arr=[['X',null,'O'],[null,'X',null],['O',null,'X']]
-arr[0][1]='O'
-console.log(arr)
+// let arr=[['X',null,'O'],[null,'X',null],['O',null,'X']]
+// arr[0][1]='O'
+// console.log(arr)
 
-let start=['jan','jul','mar','aug'];
-start.shift();
-start.splice(1,0,'jun');
-console.log(start);
+// let start=['jan','jul','mar','aug'];
+// start.shift();
+// start.splice(1,0,'jun');
+// console.log(start);
+
+// let arr=[7,9,0,-2];
+// let n=3;
+// let arr1=[7,9,0,-2];
+// console.log(arr.slice(0,n));
+// console.log(arr.slice(-n));
+
+// let ind=prompt();
+// let str="hIgHer";
+// if(str[ind]===str[ind].toLowerCase()){
+//     console.log("yes");
+// }else{
+//     console.log("no");
+// }
+
+let arr=[1,2,3,4,5];
+let num=3;
+if (arr.indexOf(num)==-1){
+    console.log("no");
+}else{
+    console.log("Yes");
+}
