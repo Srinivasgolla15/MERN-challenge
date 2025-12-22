3191c69b5226c3720d0f43d6add4a2901929de65
Srinivasgolla15 <lsrinivas0768@gmail.com>
2025-12-22 12:58:59 +0530

    day-17 ass

--- day-17/practice.js
+++ day-17/practice.js
@@ -1,26 +1,48 @@
-let max=prompt("Enter a max number");
-let rand=Math.floor(Math.random()*max)+1;
-console.log(`The random number was ${rand}`);
-let guess=prompt("Guess the number");
-console.log(`You entered ${guess}`);
-console.log(`You entered ${max}`);
+// let max=prompt("Enter a max number");
+// let rand=Math.floor(Math.random()*max)+1;
+// console.log(`The random number was ${rand}`);
+// let guess=prompt("Guess the number");
+// console.log(`You entered ${guess}`);
+// console.log(`You entered ${max}`);
 
-while(true){
+// while(true){
 
-    if (guess=="quit"){
-        console.log("You quit the game");
-        break;
-    }
-    if (guess==rand){
-        console.log("You guessed it right");
-        break;
-    }
-    guess=prompt("Guess again");
-    // console.log(rand);
-    if (guess>rand){
-        console.log("Greater");
-    }else{
-        console.log("Smaller");
-    }
+//     if (guess=="quit"){
+//         console.log("You quit the game");
+//         break;
+//     }
+//     if (guess==rand){
+//         console.log("You guessed it right");
+//         break;
+//     }
+//     guess=prompt("Guess again");
+//     // console.log(rand);
+//     if (guess>rand){
+//         console.log("Greater");
+//     }else{
+//         console.log("Smaller");
+//     }
     
+// }
+
+// let dice=Math.floor(Math.random()*6)+1;
+// console.log(dice);
+
+// const car ={
+//   color: "red",
+//   brand: "Toyota",
+//   year: 2020
+// };
+
+// console.log(car.brand);
+
+const city={
+    name:"srinu",
+    age:20,
+    city:"france"
 }
+console.log(city);
+city.city="india";
+
+console.log(city);
+
