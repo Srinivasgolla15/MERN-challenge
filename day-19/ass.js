6381e26fb25f91e385aa6c62222850c514f777af
Srinivasgolla15 <lsrinivas0768@gmail.com>
2025-12-25 19:03:28 +0530

    day-19 ass

---
+++ day-19/ass.js
@@ -0,0 +1,26 @@
+// arrayAverage=(arr) =>{
+//     let sum=0;
+//     for (let i=0;i<arr.length;i++){
+//         sum+=arr[i];
+//     }
+//     let avg=sum/arr.length;
+//     return avg;
+// }
+
+// let avg=arrayAverage([2,3,4,5]);
+// console.log(avg);
+
+
+// const Even=(n) =>{
+//     if (n%2==0){
+//         console.log("Even");
+//     }else{
+//         console.log("Odd");
+//     }
+// }
+
+// Even(5);
+
+let num=4;
+const isEven=(num)=>num%2==0;
+console.log(isEven(num));
