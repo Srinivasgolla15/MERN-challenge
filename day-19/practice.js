83a8ef15b8b98024e1d6655ebf229a7e80112127
Srinivasgolla15 <lsrinivas0768@gmail.com>
2025-12-25 18:50:14 +0530

    day-19 practice

---
+++ day-19/practice.js
@@ -0,0 +1,9 @@
+//  arrow = n =>{
+//     console.log(n*n);
+//  }
+//     arrow(5);
+
+print = str => {
+  let id = setInterval(() => { console.log(str)},2000);
+  setTimeout(() => { clearInterval(id); }, 10000);}
+print("Hello World");
