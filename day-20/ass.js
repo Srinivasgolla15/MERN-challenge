178f2b1d4a26ba398d1419e7d31f4c6569736f5c
Srinivasgolla15 <lsrinivas0768@gmail.com>
2025-12-28 23:07:37 +0530

    day-20 ass

--- day-20/ass.js
+++ day-20/ass.js
@@ -1,1 +1,16 @@
- 
+// let arr=[1,2,3,4,5];
+// let newArr=arr.map(function(el){
+//   return el+5;
+// });
+// console.log(newArr);
+
+// let arr=[1,2,3,4,5];
+// let newArr=arr.map((el)=>el*el);
+// console.log(newArr);
+// let summed=newArr.reduce((sum,el)=>sum=sum+el);
+// console.log(summed);
+// let average=summed/arr.length;
+// console.log(average);
+
+let arr=["apple", "banana", "cherry"];
+console.log(arr.map((el)=>el.toUpperCase()));
