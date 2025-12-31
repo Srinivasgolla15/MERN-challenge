7ca85ea0ffb98bade2b0a4ce1e13ea12a7d1195b
Srinivasgolla15 <lsrinivas0768@gmail.com>
2025-12-31 20:21:09 +0530

    day-22 practice

---
+++ day-22/practice.js
@@ -0,0 +1,15 @@
+let btn=document.querySelector("button");
+function getRandomColor() {
+    let red=Math.floor(Math.random()*256);
+    let green=Math.floor(Math.random()*256);
+    let blue=Math.floor(Math.random()*256);
+    return `rgb(${red}, ${green}, ${blue})`;
+}
+
+let text=document.querySelector("h1");
+let container=document.querySelector(".color-display");
+
+btn.addEventListener("click",function(){
+    container.style.backgroundColor=getRandomColor();
+    text.innerText=container.style.backgroundColor;
+})
