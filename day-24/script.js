57e8e6afd9812fac4ca0313e1cd0a42d26ac7f0d
Srinivasgolla15 <lsrinivas0768@gmail.com>
2026-01-05 17:25:53 +0530

    day-24

--- day-24/script.js
+++ day-24/script.js
@@ -1,15 +1,29 @@
-function one(){
-    return 1;
+// function one(){
+//     return 1;
 
+// }
+
+// function two(){
+//     return one()+one();
+// }
+
+// function three(){
+//     let ans=two()+one();
+//     console.log(ans);
+// }
+
+// three();
+
+let h1=document.querySelector("h1");
+function colorChange(color,delay){
+    setTimeout(()=>{
+        h1.style.color=color;
+    },delay)
 }
 
-function two(){
-    return one()+one();
-}
-
-function three(){
-    let ans=two()+one();
-    console.log(ans);
-}
-
-three();
+colorChange(red,1000)
+    .then((result)=>{
+        return colorChange(green,1000);
+    })
+    .then((result)=>{return colorChange(blue,1000);})
+    .catch
