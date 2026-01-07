7cccd63f49c10c7fc0c2759a77d54d142c5de865
Srinivasgolla15 <lsrinivas0768@gmail.com>
2026-01-07 23:12:09 +0530

    spotify changes

---
+++ day-24 copy/script.js
@@ -0,0 +1,58 @@
+// function one(){
+//     return 1;
+
+// }
+
+// function two(){
+//     return one()+one();
+// }
+
+// function three(){
+//     let ans=two()+one();
+//     console.log(ans);
+// }
+
+// three();
+
+let h1=document.querySelector("h1");
+let h2 = document.querySelector("h2");
+function colorChange(color,delay){
+    return new Promise ((resolve,reject)=>{
+        setTimeout(()=>{
+        h1.style.color=color;
+        resolve( )
+    },delay)
+    });
+    
+}
+
+colorChange("red",1000)
+    .then((result)=>{
+        return colorChange("green",1000);
+    })
+    .then((result)=>{return colorChange("blue",1000);})
+    .catch((error)=>{
+        console.log("error");
+    })
+
+function h2color(color,delay){
+    return new Promise((resolve,reject)=>{
+        setInterval(()=>{
+            setTimeout(()=>{
+            h2.style.color=color;
+            resolve()
+        },delay)
+        })
+    })
+}
+
+h2color("green",1000)
+    .then((result)=>{
+        return h2color("blue",1000);
+    })
+    .then((result)=>{
+        return h2color("red",1000);
+    })
+    .catch((error)=>{
+        console.log(error)
+    })
