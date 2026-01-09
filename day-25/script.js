e4d3fad3706fa6489183dcd75ea3f1e6fd70838e
Srinivasgolla15 <lsrinivas0768@gmail.com>
2026-01-09 15:28:12 +0530

    day-25

--- day-25/script.js
+++ day-25/script.js
@@ -82,26 +82,41 @@ colorChange("red",1000)
 
 let url="https://catfact.ninja/fact";
 
-fetch(url)
-    .then((res)=>{
-        console.log(res);
-        return res.json()
-    })
-    .then((data)=>{
-        console.log(data.fact);
-        return fetch(url)
-    })
-    .then((res)=>{
-        console.log(res);
-        return res.json()
-    })
-    .then((data1)=>{
-        console.log(data1.fact);
+// fetch(url)
+//     .then((res)=>{
+//         console.log(res);
+//         return res.json()
+//     })
+//     .then((data)=>{
+//         console.log(data.fact);
+//         return fetch(url)
+//     })
+//     .then((res)=>{
+//         console.log(res);
+//         return res.json()
+//     })
+//     .then((data1)=>{
+//         console.log(data1.fact);
          
-    })
-    .catch((err)=>{
-        console.log(err)
-    })
+//     })
+//     .catch((err)=>{
+//         console.log(err)
+//     })
+
+async function fetchurl() {
+    try{
+    let res1=await fetch(url);
+    let data1=await res1.json()
+    console.log(data1.fact)
+
+    let res2=await fetch(url);
+    let data2=await res2.json()
+    console.log(data2.fact)
+    }
+    catch (e){
+        console.log(e)
+    }
+}
 
 
 
