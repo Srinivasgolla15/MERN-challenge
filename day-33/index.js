d75deede22a805f1f8b84f4c2b4ff99ee33d6558
Srinivasgolla15 <lsrinivas0768@gmail.com>
2026-01-25 16:07:27 +0530

    day-33

--- day-33/index.js
+++ day-33/index.js
@@ -10,15 +10,18 @@ app.use(express.urlencoded({ extended: true }));
 
 let posts=[
     {
+        id:11,
         username:"john_doe",
         caption:"Enjoying the sunny day!",
         
     },
     {
+        id:2,
         username:"jane_smith",
         caption:"Loving this new recipe I tried."
         },
     {
+        id:3,
         username:"alex_92",
         caption:"Just finished a great workout!"
     }
@@ -38,6 +41,14 @@ app.get('/posts',(req,res)=>{
 app.get('/posts/new',(req,res)=>{
     res.render('form.ejs');
 });
+
+app.get('/posts/:id',(req,res)=>{
+    let {id}=req.params;
+    let post= posts.find(p=> id===p.id.toString());
+    res.render('post.ejs',{post});
+});
+
+
 
 app.post('/posts',(req,res)=>{
      let { username, caption } = req.body;
