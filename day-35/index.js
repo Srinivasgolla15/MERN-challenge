256dfe189be4c78e9d30fb811f8411f17d8d206e
Srinivasgolla15 <lsrinivas0768@gmail.com>
2026-01-28 19:51:52 +0530

    learning node with sql

--- day-35/index.js
+++ day-35/index.js
@@ -1,26 +1,34 @@
 const { faker } = require('@faker-js/faker');
 const mysql = require('mysql2');
 
+let getrandomuser = () => {
+    return [
+        faker.string.uuid(),
+        faker.person.fullName(),
+        faker.internet.email(),
+        faker.internet.password(),
+    ];
+};
+
 let connection = mysql.createConnection({
-    host:'localhost',
-    user:'root',
-    password:'Srinu@123',
+    host: 'localhost',
+    user: 'root',
+    password: 'Srinu@123',
     database: 'test'
 })
 
-connection.query("SHOW TABLES",(err,res)=>{
+let q = "INSERT INTO user (id,username,email,password) VALUES ? ";
+ 
+let users = [];
+for (let i = 0; i < 500; i++) {
+    users.push(getrandomuser());
+}
+
+connection.query(q, [users], (err, res) => {
     if (err) throw err;
     console.log(res);
 })
 
 connection.end();
-let getrandomuser = () => {
-    return {
-        id: faker.string.uuid(),
-        name: faker.person.fullName(),
-        email: faker.internet.email(),
-        password: faker.internet.password(),
-    };
-};
 
- 
+
