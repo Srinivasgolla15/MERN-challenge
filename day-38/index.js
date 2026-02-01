ca8149e51856f93a00724402571529d331a3b16a
Srinivasgolla15 <lsrinivas0768@gmail.com>
2026-02-01 21:27:08 +0530

    added chat.js

--- day-38/index.js
+++ day-38/index.js
@@ -2,6 +2,13 @@ const express = require('express');
 const app=express();
 const mongoose = require('mongoose');
 
+
+main().then(() => console.log("Connected to MongoDB")).
+catch(err=>console.log(err));
+async function main() {
+    await mongoose.connect('mondodb://127.0.0.1:27017/Whatsapp');
+}
+
 app.get("/",(req,res)=>{
     res.send("Root is Running");
 })
