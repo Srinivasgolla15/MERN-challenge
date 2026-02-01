4efdd9d5a8c247a6d5585f609b0c5d91e3cc4dc8
Srinivasgolla15 <lsrinivas0768@gmail.com>
2026-02-01 23:45:42 +0530

    day-38 with chats

--- day-38/index.js
+++ day-38/index.js
@@ -3,6 +3,9 @@ const app=express();
 const mongoose = require('mongoose');
 const path = require('path');
 const Chat = require('./chat.js');
+const methodOverride = require('method-override');
+
+app.use(methodOverride('_method'));
  
 app.use(express.urlencoded({extended:true}));
 app.set('views',path.join(__dirname,'views'));
@@ -54,6 +57,20 @@ app.get("/chats/:id/edit",async (req,res)=>{
     let chat = await Chat.findById(id);
     res.render('edit.ejs',{chat});
 });
+
+app.put("/chats/:id",async (req,res)=>{
+    let {id} = req.params;
+    let {msg} = req.body;
+    await Chat.findByIdAndUpdate(id,{msg:msg},{runValidator:true,new:true});
+    res.redirect("/chats");
+});
+
+app.delete("/chats/:id",async (req,res)=>{
+    let {id} = req.params;
+    await Chat.findByIdAndDelete(id);
+    res.redirect("/chats");
+});
+
 
 app.listen(3000,()=>{
     console.log("Server is running on port 3000");
