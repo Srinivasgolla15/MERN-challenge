2ccda88cf11812f218553c96ddfd2734deb1f7fa
Srinivasgolla15 <lsrinivas0768@gmail.com>
2026-02-02 20:15:50 +0530

    authentication with MVC architecture

--- auth/index.js
+++ auth/index.js
@@ -4,7 +4,11 @@ const mongoose = require('mongoose');
 const methodOverride = require('method-override');
 const userRoutes = require('./routes/user');
 const path = require('path');
-
+const cookieParser = require('cookie-parser');
+app.use(cookieParser());
+const staticRoutes = require('./routes/staticRoutes');
+app.use(staticRoutes);
+const {restrictToLoggedinUserOnly} = require("./middlewares/auth");
 async function main(){
     await mongoose.connect('mongodb://127.0.0.1:27017/AuthDB');
     console.log("Connected to MongoDB");
@@ -13,7 +17,7 @@ async function main(){
 main().catch(err=>console.log(err));
 
 app.use(methodOverride('_method'));
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({extended:false}));
 app.set('view engine', 'ejs');
 app.set('views',path.join(__dirname,'views'));
 
