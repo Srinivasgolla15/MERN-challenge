31fdc3ad8df9a59170e0a7e372ea5dd79471337b
Srinivasgolla15 <lsrinivas0768@gmail.com>
2026-02-22 13:46:48 +0530

    user models

---
+++ majorproject/models/user.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const passportLocalMongoose = require("passport-local-mongoose");
+
+const userSchema = new Schema({
+    username:String,
+    password:String,
+    email:{type:String, required:true},
+    createdAt : {
+        type:Date,
+        default:Date.now()
+    }
+});
+userSchema.plugin(passportLocalMongoose);
