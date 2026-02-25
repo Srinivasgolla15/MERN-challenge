d0cf0fa6c1d14b7482cef0046ad24154aa278fce
Srinivasgolla15 <lsrinivas0768@gmail.com>
2026-02-25 12:27:31 +0530

    blogapp

--- blogapp/app.js
+++ blogapp/app.js
@@ -14,7 +14,7 @@ const app = express();
 
 main().catch(err => { console.log(err); });
 async function main() {
-    await mongoose.connect("process.env.MONGO_URL");
+    await mongoose.connect(process.env.MONGO_URL);
     console.log("Connected to MongoDB");
 }
 
