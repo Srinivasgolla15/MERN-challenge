6877deb865629ea64825db05bfcd3990b3a99863
Srinivasgolla15 <lsrinivas0768@gmail.com>
2026-03-10 15:36:39 +0530

    react day-3

--- react-2/src/msgBox.jsx
+++ react-2/src/msgBox.jsx
@@ -1,7 +1,19 @@
+import { useState } from "react"
+
 export default function MsgBox({userName,textColor}) {
+
+    let [isLiked,setIsLiked]= useState(false)
+
+    let likeHandler=()=>{
+        setIsLiked(!isLiked)
+    }
+
+     
+    
     return (
         <div>
             <h1 >Hello <span style={{color:textColor}}>{userName}</span>!</h1>
+            <p onClick={likeHandler}>{isLiked?<i class="fa-solid fa-heart"></i>:<i class="fa-regular fa-heart"></i>} </p>
         </div>
     )
 }
